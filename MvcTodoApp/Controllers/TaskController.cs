157ea08189cf615b389466d3c6880ddd6a80d919
unknown <asusithaw@gmail.com>
2024-06-28 16:34:30 +0530

    first commit

---
+++ MvcTodoApp/Controllers/TaskController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTodoApp.Entities;
+using NHibernate.Linq;
+using MvcTodoApp.ViewModels;
+
+namespace MvcTodoApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        //
+        // GET: /Task/
+
+        public ActionResult Index()
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                var tasks = session.Query<Task>().ToList();
+                var viewModel = new TaskViewModel
+                {
+                    Tasks = tasks,
+                    TotalTasks = tasks.Count
+                };
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult AddTask(string description)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    var task = new Task { Description = description };
+                    session.Save(task);
+                    transaction.Commit();
+                    return Json(task);
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    return Json(new { success = false, message = ex.Message });
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult RemoveTask(int id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    var task = session.Get<Task>(id);
+                    if (task != null)
+                    {
+                        session.Delete(task);
+                        transaction.Commit();
+                        return Json(new { success = true });
+                    }
+                    return Json(new { success = false, message = "Task not found" });
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    return Json(new { success = false, message = ex.Message });
+                }
+            }
+        }
+    }
+}
---
+++ MvcTodoApp/ViewModels/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcTodoApp.Entities;
+
+namespace MvcTodoApp.ViewModels
+{
+    public class TaskViewModel
+    {
+        public IEnumerable<Task> Tasks { get; set; }
+        public int TotalTasks { get; set; }
+    }
+}
